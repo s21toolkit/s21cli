66cd27329d09c232d4178d99a28733ff751c9ad5
ebsviridov <sviridov.e.b@sbertech.ru>
2023-10-18 13:04:12 +0300

    build: add new build script

---
+++ build.mjs
@@ -0,0 +1,7 @@
+const { platform, arch } = process
+
+const outfile = `build/s21-${platform}-${arch}`
+
+Bun.spawnSync({
+	cmd: ["bun", "build", "--compile", "./bin/index.ts", "--minify", "--outfile", outfile]
+})
