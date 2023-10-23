5fc2eb85fdcc395476b28f651ab38c45b644ad67
bgenia <bgenia@student.21-school.ru>
2023-10-23 21:24:37 +0300

    feat: implement command error handling

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -2,9 +2,19 @@ import { type } from "arktype"
 import { ConfigurationManager } from "./ConfigurationManager"
 
 export const Configuration = new ConfigurationManager({
-	prDirectory: type("string"),
+	// Authentication
 	username: type("string"),
 	password: type("string"),
+
+	// Debug
+	debugDisableErrorHandling: type([
+		"boolean",
+		"|",
+		["string", "|>", (value) => value === "true"],
+	]),
+
+	// pr/clone
+	prDirectory: type("string"),
 })
 
 await Configuration.load()
