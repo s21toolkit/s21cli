e6db92cf431b88d0fd850c47a457d228da0c24b3
EnergoStalin <ens.stalin@gmail.com>
2023-11-19 21:33:10 +0300

    feat: watcher

--- src/cli/arguments/duration.ts
+++ src/cli/arguments/duration.ts
@@ -1,29 +1,30 @@
 import { extendType, string } from "cmd-ts"
 
 const multiplyers: Record<string, number> = {
-    "y": 60 * 60 * 24 * 365,
-    "M": 60 * 60 * 24 * 31,
-    "w": 60 * 60 * 24 * 7,
-    "d": 60 * 60 * 24,
-    "h": 60 * 60,
-    "m": 60,
-    "s": 1,
+	"🫦": 60 * 60 * 24 * 365 * 100,
+	"y": 60 * 60 * 24 * 365,
+	"M": 60 * 60 * 24 * 31,
+	"w": 60 * 60 * 24 * 7,
+	"d": 60 * 60 * 24,
+	"h": 60 * 60,
+	"m": 60,
+	"s": 1,
 }
 
 export const duration = extendType(string, {
 	displayName: "duration",
-	description: "Duration in supporting w,d,h,m,s multiplyers and returning the seconds in it",
+	description: "Duration supporting w,d,h,m,s multiplyers and returning the seconds in it",
 	async from(rawValue) {
-        let multiplyer = multiplyers[rawValue[rawValue.length - 1]!];
-        if(!multiplyer) {
-            throw new Error("Unknow multiplyer specified")
-        }
+		let multiplyer = multiplyers[rawValue[rawValue.length - 1]!];
+		if(!multiplyer) {
+			throw new Error("Unknow multiplyer specified")
+		}
 
-        const parsed = parseInt(rawValue.slice(0, rawValue.length - 1), 10)
-        if(parsed != parsed) {
-            throw new Error("Cant parse number")
-        }
+		const parsed = parseInt(rawValue.slice(0, rawValue.length - 1), 10)
+		if(parsed != parsed) {
+			throw new Error("Cant parse number")
+		}
 
-        return parsed * multiplyer
+		return parsed * multiplyer
 	},
 })
--- src/cli/commands/wfs/index.ts
+++ src/cli/commands/wfs/index.ts
@@ -1,10 +1,8 @@
 import { command, option } from "cmd-ts"
 import { duration } from "@/cli/arguments/duration"
-import { getNodeCode } from "@/git/getNodeCode"
-import { Client, DefaultAuthProvider } from "@s21toolkit/client"
-import { Configuration } from "@/configuration"
-import { getGoalIdFromNodeCode } from "@/adapters/getGoalIdFromNodeCode"
 import { resolveGoalIdFromGit } from "@/git/resolveGoalIdFromGit"
+import { getAuthorizedClient } from "@/auth"
+import dayjs from "dayjs"
 
 export const watchForSlots = command({
 	aliases: ["watch", "wfs", "watchForSlots"],
@@ -19,6 +17,36 @@ export const watchForSlots = command({
 		})
 	},
 	async handler (args) {
-		console.log(await resolveGoalIdFromGit())
+		const client = getAuthorizedClient();
+
+		const module = await client.api.calendarGetModule({
+			moduleId: await resolveGoalIdFromGit()
+		})
+
+		const taskId = module.student.getModuleById.currentTask.task.id
+
+		console.log("Watching...")
+
+		while(true) {
+			const slots = await client.api.calendarGetNameLessStudentTimeslotsForReview({
+				from: dayjs().toDate(),
+				to: dayjs().add(args.ahead, "s").toDate(),
+				taskId: taskId
+			})
+
+			const timeSlots = slots.student.getNameLessStudentTimeslotsForReview.timeSlots
+			if(timeSlots.length === 0) {
+				continue
+			}
+
+			client.api.calendarAddBookingToEventSlot({
+				answerId: module.student.getModuleById.trajectory.levels[0]!.goalElements[0]!.points[0]!.studentTask.lastAnswer.id,
+				startTime: timeSlots[0]!.validStartTimes[0]!.toISOString(),
+				wasStaffSlotChosen: timeSlots[0]!.staffSlot.toString(),
+				isOnline: false
+			})
+
+			console.log(`Subscribed on slot ${timeSlots[0]!.validStartTimes[0]}`)
+		}
 	}
 })
