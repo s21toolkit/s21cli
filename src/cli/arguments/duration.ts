67429694d4673ce2904d3ec4e4d389d4076342cd
bgenia <bgenia@student.21-school.ru>
2023-11-20 02:46:41 +0300

    refactor: refactor duraction argument type

--- src/cli/arguments/duration.ts
+++ src/cli/arguments/duration.ts
@@ -1,30 +1,31 @@
 import { extendType, string } from "cmd-ts"
 
-const multiplyers: Record<string, number> = {
+const multipliers: Record<string, number> = {
 	"🫦": 60 * 60 * 24 * 365 * 100,
-	"y": 60 * 60 * 24 * 365,
-	"M": 60 * 60 * 24 * 31,
-	"w": 60 * 60 * 24 * 7,
-	"d": 60 * 60 * 24,
-	"h": 60 * 60,
-	"m": 60,
-	"s": 1,
+	y: 60 * 60 * 24 * 365,
+	M: 60 * 60 * 24 * 31,
+	w: 60 * 60 * 24 * 7,
+	d: 60 * 60 * 24,
+	h: 60 * 60,
+	m: 60,
+	s: 1,
 }
 
 export const duration = extendType(string, {
 	displayName: "duration",
-	description: "Duration supporting w,d,h,m,s multiplyers and returning the seconds in it",
+	description:
+		"Duration supporting w,d,h,m,s multipliers and returning the seconds in it",
 	async from(rawValue) {
-		let multiplyer = multiplyers[rawValue[rawValue.length - 1]!];
-		if(!multiplyer) {
-			throw new Error("Unknow multiplyer specified")
+		const multiplier = multipliers[rawValue[rawValue.length - 1]!]
+		if (!multiplier) {
+			throw new Error("Unknown multiplier specified")
 		}
 
-		const parsed = parseInt(rawValue.slice(0, rawValue.length - 1), 10)
-		if(parsed != parsed) {
-			throw new Error("Cant parse number")
+		const parsed = Number.parseInt(rawValue.slice(0, -1), 10)
+		if (Number.isNaN(parsed)) {
+			throw new TypeError("Cant parse number")
 		}
 
-		return parsed * multiplyer
+		return parsed * multiplier
 	},
 })
