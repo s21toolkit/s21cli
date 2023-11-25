eb60651a4ca547e028b913362b4c4fcc9fc4c388
bgenia <bgenia@student.21-school.ru>
2023-11-25 23:55:50 +0300

    build: remove version definitions from build script, also remove minification

--- bun/build.ts
+++ bun/build.ts
@@ -6,15 +6,11 @@ const { platform, arch } = process
 const entrypoint = resolve("bin/index.ts")
 const outfile = resolve(`build/s21-${platform}-${arch}`)
 
-const version = process.env.APP_VERSION ?? "dev"
-
 Bun.spawnSync({
 	cmd: [
 		"bun",
 		"build",
 		...["--compile", entrypoint],
-		...["--define", `EXTERNAL_APP_VERSION="${version}"`],
-		"--minify",
 		...["--outfile", outfile],
 	],
 	stdio: ["inherit", "inherit", "inherit"],
