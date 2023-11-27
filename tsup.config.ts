6f0127c64df487f6c40c275182dcfa63f959bfd4
bgenia <bgenia@student.21-school.ru>
2023-11-28 01:39:17 +0300

    fix: disable broken runtime dependency bundling

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,4 @@
 import { defineConfig } from "tsup"
-import packageJson from "@root/package.json"
-
-const runtimeDependencies = Object.keys(packageJson.dependencies)
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
@@ -11,7 +8,5 @@ export default defineConfig({
 	format: "esm",
 	target: "node20",
 	outDir: "build/dist",
-	noExternal: [...runtimeDependencies, "util"],
-	treeshake: true,
 	outExtension: () => ({ js: ".mjs" })
 })
