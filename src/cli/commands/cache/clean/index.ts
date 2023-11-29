4e03fd3e15e9fb34bc8335c7870a5dfebb5f0ba9
bgenia <bgenia@student.21-school.ru>
2023-11-29 05:45:58 +0300

    feat: implement cache/clean flags

--- src/cli/commands/cache/clean/index.ts
+++ src/cli/commands/cache/clean/index.ts
@@ -1,16 +1,53 @@
-import { command } from "cmd-ts"
+import { command, flag } from "cmd-ts"
 import { rm } from "node:fs/promises"
-import { cacheDirectory } from "@/cache"
+import {
+	cacheDirectory,
+	globalCacheDirectory,
+	localCacheDirectory,
+} from "@/cache"
+
+async function cleanCache(directory: string) {
+	console.log(`Cleaning cache from '${directory}' . . .`)
+
+	await rm(directory, { force: true, recursive: true })
+
+	console.log("Complete.")
+}
 
 export const cleanCommand = command({
 	name: "clean",
 	description: "Clean cache",
-	args: {},
-	async handler() {
-		console.log(`Cleaning cache from '${cacheDirectory}' . . .`)
+	args: {
+		local: flag({
+			long: "local",
+			short: "l",
+			description: "Clean local cache (ignores configuration)",
+			defaultValue: () => false,
+		}),
+		global: flag({
+			long: "global",
+			short: "g",
+			description: "Clean global cache (ignores configuration)",
+			defaultValue: () => false,
+		}),
+		all: flag({
+			long: "all",
+			short: "a",
+			description: "Clean both local & global cache (ignores configuration)",
+			defaultValue: () => false,
+		}),
+	},
+	async handler(argv) {
+		if (argv.local || argv.all) {
+			await cleanCache(localCacheDirectory)
+		}
 
-		await rm(cacheDirectory, { force: true, recursive: true })
+		if (argv.global || argv.all) {
+			await cleanCache(globalCacheDirectory)
+		}
 
-		console.log("Complete.")
+		if (!argv.global && !argv.local && !argv.all) {
+			await cleanCache(cacheDirectory)
+		}
 	},
 })
