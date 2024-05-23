2f189247fec232407ec7650a52095711cbdfa34d
bgenia <xbgenia@gmail.com>
2024-05-23 18:58:46 +0300

    feat: implement cross-compilation

--- compile.ts
+++ compile.ts
@@ -1,19 +1,41 @@
-import { spawnSync } from "node:child_process"
+import { spawn } from "node:child_process"
 import { resolve } from "node:path"
-import process from "node:process"
 
-const { platform, arch } = process
+const targets = [
+	"linux-x64",
+	"linux-arm64",
+	"darwin-x64",
+	"darwin-arm64",
+	"windows-x64",
+]
 
 const entrypoint = resolve("build/dist/main.mjs")
-const outfile = resolve(`build/bin/s21-${platform}-${arch}`)
 
-spawnSync("pnpm", [
-	"bun",
-	"build",
-	"--compile",
-	entrypoint,
-	"--define",
-	"__BINARY_BUILD=true",
-	"--outfile",
-	outfile,
-])
+const buildProcesses = []
+
+for (const target of targets) {
+	const outfile = resolve(`build/bin/s21-${target}`)
+
+	const process = spawn("pnpm", [
+		"bun",
+		"build",
+		"--compile",
+		entrypoint,
+		"--target",
+		`bun-${target}`,
+		"--minify",
+		"--sourcemap",
+		"--define",
+		"__BINARY_BUILD=true",
+		"--outfile",
+		outfile,
+	])
+
+	buildProcesses.push(process)
+}
+
+await Promise.all(
+	buildProcesses.map(
+		(process) => new Promise((resolve) => process.on("close", resolve)),
+	),
+)
